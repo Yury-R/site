6c88cfe10976f96abb6a513b4551e35e28ae9fc2
Yury Rybianets <rybianets.y@gmail.com>
2021-03-09 12:39:32 +0300

    misc.

--- user_profile/forms.py
+++ user_profile/forms.py
@@ -15,11 +15,12 @@ class AuthUserForm(AuthenticationForm, forms.ModelForm):
 
 
 class RegisterUserForm(forms.ModelForm):
+
     class Meta:
         model = User
         fields = ('username', 'password')
 
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
